b7dba85e963967c01090b7eb651bff9ae8da9402
John Mandalakas <john@mandalakas.com>
2016-09-14 23:34:55 -0500

    Add a few commands

--- src/yfs_bot/core.clj
+++ src/yfs_bot/core.clj
@@ -4,9 +4,59 @@
             [yfs-bot.telegram :as tg]
             [yfs-bot.persistence :as db]))
 
+(defn call
+  "function that calls another function if it exists"
+  [fn-name & args]
+  (when (and fn-name (resolve (symbol fn-name)))
+    (apply (resolve (symbol fn-name)) args)))
+
+(defn get-command
+  [message]
+  (let [entity
+        (first (filter #(= (:type %) "bot_command") (:entities message)))]
+    (when entity
+      (subs
+       (:text message)
+       (inc (:offset entity))
+       (+ (:offset entity) (:length entity))))))
+
 (defn handle-update
+  "function for handling updates"
   [update]
-  (println update))
+  (do
+    (println update)
+    (call (get-command (:message update))
+          (:message update))))
+
+(defn start
+  "telegram's default start command"
+  [message]
+  (tg/send-message
+   (:id (:from message))
+   (str
+    "To start, grab a code from [here]("
+    auth/auth-url
+    ")\nthen send it to me with `/register <code>`")))
+
+(defn register
+  "command to register a new user"
+  [message]
+  (auth/get-token
+   (last (clojure.string/split (:text message) #" ")))
+  (tg/send-message
+   (:id (:from message))
+   (str
+    "OK, You're authenticated!")))
+
+(defn help
+  "command to print some help messages"
+  [message]
+  (tg/send-message
+   (:id (:from message))
+   (str
+     "/start - start using the bot \n"
+     "/register - send us a yahoo code to verify your account \n"
+     "/help - get this message \n")))
 
 ;; |  Λ--Λ--Λ--Λ--Λ--Λ  |
 ;; | [o][o][o][o][o][o] |
@@ -35,7 +85,7 @@
   [& args]
   (while true
    (do
-    (println @offset)
+    ; (println @offset)
     (let [last-update-id (poll-for-and-process-updates)]
       (when last-update-id (reset! offset (inc last-update-id)))))
     (Thread/sleep 800)))
